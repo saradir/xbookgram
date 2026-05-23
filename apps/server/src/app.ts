ac9643527d8abd39dac1527e9fc21b10a9192d89
saradir <120840462+saradir@users.noreply.github.com>
2026-05-23 19:55:38 +0300

    feat: add JWT support for Google authentication and update type definitions

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction } from 'express';
 import passport from 'passport';
 import './config/passport.js';
-import { ErrorRequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
 import { errorHandler } from './middlewares/middleware.js';
 import { corsConfig } from './config/cors.js';
 import cors from "cors"; 
@@ -13,7 +13,35 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors(corsConfig));
 
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+app.get("/auth/google",
+     passport.authenticate("google", {
+         scope: ["profile", "email"],
+         prompt: 'select_account'
+         }));
+  
+app.get(
+    "/auth/google/callback",
+    passport.authenticate('google', { session: false, failureRedirect: "http://localhost:5173/login?error=auth_failed"}),
+    (req, res) => {
+        const user = req.user as any;
+
+        const token = jwt.sign(
+            { userId: user.id, email: user.email },
+            process.env.JWT_SECRET!,
+            { expiresIn: '30d'}
+        );
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'lax',
+            maxAge: 30 * 24 * 60 * 60 * 1000
+        });
+
+        res.redirect('http://localhost:5173/dashboard');
+    }
+);
+
 app.use(errorHandler);
 
 app.listen(PORT, ()=> {
