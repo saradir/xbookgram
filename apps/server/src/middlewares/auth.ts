6dbb7174bf0449d4977c23758b23d53f9564bed6
saradir <120840462+saradir@users.noreply.github.com>
2026-05-24 22:45:32 +0300

    feat: add authentication middleware to protect routes with JWT

---
+++ apps/server/src/middlewares/auth.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+
+
+
+export const protect: RequestHandler = (req, res, next) => {
+
+    let token: string | null = null;
+
+    if(req.cookies && req.cookies.token) {
+        token = req.cookies.token;
+    }
+
+    else if( req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if(!token){
+        return res.status(401).json({ error: "Not authorized: no token provided"});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { userId: string};
+        req.userId = decoded.userId;
+        next();
+    } catch (err){
+        return res.status(401).json({ error: "Not authorized: token invalid"});
+    }
+
+}
