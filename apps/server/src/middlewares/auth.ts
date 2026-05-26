c48bf2528a2688e5e276773e8d01575bc41f1261
saradir <120840462+saradir@users.noreply.github.com>
2026-05-26 16:01:21 +0300

    fix: change function name and change userId to number

--- apps/server/src/middlewares/auth.ts
+++ apps/server/src/middlewares/auth.ts
@@ -1,30 +1,29 @@
-import { RequestHandler } from "express";
-import jwt from "jsonwebtoken";
+import { RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
 
+export const authenticate: RequestHandler = async (req, res, next) => {
+  let token: string | null = null;
 
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  } else if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
 
-export const protect: RequestHandler = (req, res, next) => {
+  if (!token) {
+    return res.status(401).json({ error: 'Not authorized: no token provided' });
+  }
 
-    let token: string | null = null;
-
-    if(req.cookies && req.cookies.token) {
-        token = req.cookies.token;
-    }
-
-    else if( req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        token = req.headers.authorization.split(' ')[1];
-    }
-
-    if(!token){
-        return res.status(401).json({ error: "Not authorized: no token provided"});
-    }
-
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { userId: string};
-        req.userId = decoded.userId;
-        next();
-    } catch (err){
-        return res.status(401).json({ error: "Not authorized: token invalid"});
-    }
-
-}
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      userId: string;
+    };
+    req.userId = Number(decoded.userId);
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Not authorized: token invalid' });
+  }
+};
