0a7c096b61e4b394e251b7c73c75f4533d0021f3
saradir <120840462+saradir@users.noreply.github.com>
2026-05-29 18:32:21 +0300

    fix: correct import name

--- apps/server/src/routers/auth.ts
+++ apps/server/src/routers/auth.ts
@@ -1,44 +1,45 @@
-import { Router } from "express";
-import passport from "passport";
+import { Router } from 'express';
+import passport from 'passport';
 import jwt from 'jsonwebtoken';
-import { protect } from "../middlewares/auth.js";
+import { authenticate } from '../middlewares/auth.js';
 
 export const authRouter = Router();
 
-authRouter.get("/google",
-     passport.authenticate("google", {
-         scope: ["profile", "email"],
-         prompt: 'select_account'
-         }));
-  
 authRouter.get(
-    "/google/callback",
-    passport.authenticate('google', { session: false, failureRedirect: "http://localhost:5173/login?error=auth_failed"}),
-    (req, res) => {
-        const user = req.user as any;
-
-        const token = jwt.sign(
-            { userId: user.id, email: user.email },
-            process.env.JWT_SECRET!,
-            { expiresIn: '30d'}
-        );
-
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
-            maxAge: 30 * 24 * 60 * 60 * 1000
-        });
-
-        res.redirect('http://localhost:5173/dashboard');
-    }
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    prompt: 'select_account',
+  })
 );
 
 authRouter.get(
-    "/me",
-    protect,
-    (req, res, next) => {
-        console.log("Valid request. User is:", req.userId);
-        res.json({ message: `Welcome user ${req.userId}` });
-    }
-)
+  '/google/callback',
+  passport.authenticate('google', {
+    session: false,
+    failureRedirect: 'http://localhost:5173/login?error=auth_failed',
+  }),
+  (req, res) => {
+    const user = req.user as any;
+
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: '30d' }
+    );
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    res.redirect('http://localhost:5173/dashboard');
+  }
+);
+
+authRouter.get('/me', authenticate, (req, res, next) => {
+  console.log('Valid request. User is:', req.userId);
+  res.json({ message: `Welcome user ${req.userId}` });
+});
