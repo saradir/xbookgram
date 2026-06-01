156efcb3dffea1343d948107f81151170a3fe939
saradir <120840462+saradir@users.noreply.github.com>
2026-06-01 16:14:42 +0300

    fix: add checks to prevent infinite redirecting to '/onboarding' when user isn't onboarded, and prevent onboarded users from accessing '/onboarding'.

--- apps/client/src/components/ProtectedRoute.tsx
+++ apps/client/src/components/ProtectedRoute.tsx
@@ -1,13 +1,17 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { useLocation } from 'react-router-dom';
 
 export function ProtectedRoute() {
+  const location = useLocation();
   const { currentUser, isLoading } = useCurrentUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (!currentUser) return <Navigate to="/login" replace />;
-  if (!currentUser.user.isOnboarded)
+  if (!currentUser.user.isOnboarded && location.pathname !== '/onboarding')
     return <Navigate to="/onboarding" replace />;
+  if (currentUser.user.isOnboarded && location.pathname === '/onboarding')
+    return <Navigate to="/" replace />;
 
   return <Outlet />;
 }
