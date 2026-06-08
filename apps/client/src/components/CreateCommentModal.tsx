3c75db956a3fa53adb80610ef8007f69328c9385
saradir <120840462+saradir@users.noreply.github.com>
2026-06-08 16:45:40 +0300

    feat: implement comment modal

---
+++ apps/client/src/components/CreateCommentModal.tsx
@@ -0,0 +1,72 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Field, FieldGroup } from '@/components/ui/field';
+import { Button } from './ui/button';
+import { Textarea } from './ui/textarea';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useCreateComment } from '@/hooks/useCreateComment';
+
+export function CreateCommentModal({
+  postId,
+  open,
+  setOpen,
+}: {
+  postId: number;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
+  const { mutate, isPending } = useCreateComment(postId);
+  const [content, setContent] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    if (!content.trim()) return;
+    mutate(content, {
+      onSuccess: () => {
+        setOpen(false);
+        setContent('');
+        navigate(`/posts/${postId}`);
+      },
+    });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <form>
+        <DialogContent className="sm:max-w-sm">
+          <DialogHeader>
+            <DialogTitle>Add Comment</DialogTitle>
+            <DialogDescription>Share your reaction</DialogDescription>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Textarea
+                id="content"
+                rows={15}
+                name="content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                className="resize-none  shadow-none focus-visible:ring-0"
+              />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="button" disabled={isPending} onClick={handleSubmit}>
+              Comment
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
--- apps/client/src/components/PostCard.tsx
+++ apps/client/src/components/PostCard.tsx
@@ -6,65 +6,71 @@ import {
 } from '@/components/ui/card';
 import type { Post } from '@xbookgram/shared';
 import { SharedPostPreview } from './SharedPostPreview';
-import { Heart, MessageCircle, Repeat2 } from 'lucide-react';
+import { Heart, Repeat2, MessageCircle } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useToggleLike } from '@/hooks/useToggleLike';
+import { CreateCommentModal } from './CreateCommentModal';
+import { useState } from 'react';
 
 export function PostCard({ post }: { post: Post }) {
+  const [openModal, setOpen] = useState(false);
   const { mutate } = useToggleLike('post', post.id);
 
   return (
-    <Card>
-      <CardHeader className="flex items-center gap-2">
-        <Link
-          to={`/users/${post.author.id}`}
-          className="flex items-center gap-2"
-        >
-          <div className="flex items-center justify-center h-10 w-10 rounded-full bg-blue-500">
-            {post.author.username[0].toUpperCase()}
+    <div>
+      <CreateCommentModal postId={post.id} setOpen={setOpen} open={openModal} />
+      <Card>
+        <CardHeader className="flex items-center gap-2">
+          <Link
+            to={`/users/${post.author.id}`}
+            className="flex items-center gap-2"
+          >
+            <div className="flex items-center justify-center h-10 w-10 rounded-full bg-blue-500">
+              {post.author.username[0].toUpperCase()}
+            </div>
+            <div className="font-bold">{post.author.username}</div>
+          </Link>
+          <div className="ml-auto text-muted-foreground">
+            {new Date(post.createdAt).toLocaleDateString('en-US', {
+              month: 'short',
+              day: 'numeric',
+            })}
           </div>
-          <div className="font-bold">{post.author.username}</div>
-        </Link>
-        <div className="ml-auto text-muted-foreground">
-          {new Date(post.createdAt).toLocaleDateString('en-US', {
-            month: 'short',
-            day: 'numeric',
-          })}
-        </div>
-      </CardHeader>
-      <CardContent className="flex flex-col pl-10 gap-3">
-        <Link to={`/posts/${post.id}`}>
-          {post.content && <p>{post.content}</p>}
-        </Link>
-        {post.originalPost && <SharedPostPreview post={post.originalPost} />}
-      </CardContent>
-
-      <CardFooter className="flex gap-6 pl-10 py-2">
-        <div
-          className="flex gap-1 items-center cursor-pointer"
-          onClick={() => mutate()}
-        >
-          <Heart
-            size={16}
-            className={
-              post.isLiked
-                ? 'fill-red-500 text-red-500'
-                : 'text-muted-foreground'
-            }
-          />
-          {post._count.likes > 0 && <span>{post._count.likes}</span>}
-        </div>
-        <Link to={`/posts/${post.id}`}>
-          <div className="flex gap-1  items-center  text-muted-foreground ">
+        </CardHeader>
+        <CardContent className="flex flex-col pl-10 gap-3">
+          <Link to={`/posts/${post.id}`}>
+            {post.content && <p>{post.content}</p>}
+          </Link>
+          {post.originalPost && <SharedPostPreview post={post.originalPost} />}
+        </CardContent>
+        <CardFooter className="flex gap-2 pl-10 py-2">
+          <div
+            className="flex gap-1 items-center cursor-pointer text-muted-foreground hover:text-foreground hover:bg-muted rounded-md  py-1"
+            onClick={() => mutate()}
+          >
+            <Heart
+              size={16}
+              className={
+                post.isLiked
+                  ? 'fill-red-500 text-red-500'
+                  : 'text-muted-foreground'
+              }
+            />
+            {post._count.likes > 0 && <span>{post._count.likes}</span>}
+          </div>
+          <div
+            className="flex gap-1 items-center cursor-pointer text-muted-foreground hover:text-foreground hover:bg-muted rounded-md px-2 py-1"
+            onClick={() => setOpen(true)}
+          >
             <MessageCircle size={16} />
-            {post._count.likes > 0 && <span>{post._count.comments} </span>}
+            {post._count.comments > 0 && <span>{post._count.comments}</span>}
           </div>
-        </Link>
-        <div className="flex gap-1  items-center text-muted-foreground ">
-          <Repeat2 size={16} />
-          {post._count.sharedBy > 0 && <span>{post._count.sharedBy} </span>}
-        </div>
-      </CardFooter>
-    </Card>
+          <div className="flex gap-1 items-center cursor-pointer text-muted-foreground hover:text-foreground hover:bg-muted rounded-md px-2 py-1 ">
+            <Repeat2 size={16} />
+            {post._count.sharedBy > 0 && <span>{post._count.sharedBy} </span>}
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
