d1c9e7ffcd91933c134e41e912a12ac9a6a68bf3
saradir <120840462+saradir@users.noreply.github.com>
2026-06-11 15:14:23 +0300

    fix: change type of fetch function

--- apps/client/src/hooks/useSearch.ts
+++ apps/client/src/hooks/useSearch.ts
@@ -1,11 +1,12 @@
 import { apiFetch } from '@/lib/api';
 import { useQuery } from '@tanstack/react-query';
-import type { User } from '@xbookgram/shared';
+import type { UserProfile } from '@xbookgram/shared';
 
 export function useSearch(query: string) {
   const { data, isLoading, isError } = useQuery({
     queryKey: ['search', query],
-    queryFn: () => apiFetch<{ users: User[] }>(`/api/users/search?q=${query}`),
+    queryFn: () =>
+      apiFetch<{ users: UserProfile[] }>(`/api/users/search?q=${query}`),
     enabled: !!query && query.length > 2,
     retry: false,
   });
