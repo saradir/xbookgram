3fc8fde5ab8ac90610bf82c1bb94194efd8af356
saradir <120840462+saradir@users.noreply.github.com>
2026-06-11 15:33:20 +0300

    feat: wire search button

--- apps/client/src/components/SearchbarLive.tsx
+++ apps/client/src/components/SearchbarLive.tsx
@@ -6,44 +6,52 @@ import { Button } from './ui/button';
 import { useSearch } from '@/hooks/useSearch';
 import { useDebounce } from '@/hooks/useDebounce';
 import { ResultRowLive } from './ResultRowLive';
+import { useNavigate } from 'react-router-dom';
 
 export function SearchbarLive() {
   const [query, setQuery] = useState('');
   const debouncedQuery = useDebounce(query, 1000);
   const { users } = useSearch(debouncedQuery);
   const [showResults, setShowResults] = useState(false);
-
+  const navigate = useNavigate();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
     if (e.target.value.length > 2) setShowResults(true);
   };
-  return (
-    <Field
-      orientation="horizontal"
-      className="relative border-none bg-muted rounded-md"
-    >
-      <Input
-        type="search"
-        placeholder="Type to search"
-        value={query}
-        onChange={handleChange}
-        className="focus-visible:ring-0 focus:outline-none border-none"
-        onBlur={() => setShowResults(false)}
-        onFocus={() => {
-          if (query.length > 2) setShowResults(true);
-        }}
-      />
-      {showResults && (
-        <div className="absolute top-full left-0 w-full z-50 bg-background  rounded-md shadow-md">
-          {users?.map((u) => (
-            <ResultRowLive key={u.id} user={u} />
-          ))}
-        </div>
-      )}
 
-      <Button className="cursor-pointer" variant={'ghost'}>
-        <Search />
-      </Button>
-    </Field>
+  const onSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate(`/search?q=${query}`);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Field
+        orientation="horizontal"
+        className="relative border-none bg-muted rounded-md"
+      >
+        <Input
+          type="search"
+          placeholder="Type to search"
+          value={query}
+          onChange={handleChange}
+          className="focus-visible:ring-0 focus:outline-none border-none"
+          onBlur={() => setShowResults(false)}
+          onFocus={() => {
+            if (query.length > 2) setShowResults(true);
+          }}
+        />
+        {showResults && (
+          <div className="absolute top-full left-0 w-full z-50 bg-background  rounded-md shadow-md">
+            {users?.map((u) => (
+              <ResultRowLive key={u.id} user={u} />
+            ))}
+          </div>
+        )}
+        <Button className="cursor-pointer" variant={'ghost'} type="submit">
+          <Search />
+        </Button>
+      </Field>
+    </form>
   );
 }
