f8eb0ebcf63e76ed4c5035f976f2681080b86704
saradir <120840462+saradir@users.noreply.github.com>
2026-06-11 22:15:26 +0300

    feat: create notification type

---
+++ packages/shared/src/types/notification.types.ts
@@ -0,0 +1,13 @@
+import { User } from './user.types.js';
+
+export type Notification = {
+  id: number;
+  actorId: number;
+  receipientId: number;
+  isRead: boolean;
+  createdAt: string;
+  type: 'LIKE' | 'SHARE' | 'FOLLOW' | 'COMMENT';
+  actor: User | null;
+  postId: number | null;
+  commentId: number | null;
+};
