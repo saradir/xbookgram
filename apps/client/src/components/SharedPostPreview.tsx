42a52696aa4307cea00e3e263a0800a1bed66492
saradir <120840462+saradir@users.noreply.github.com>
2026-06-13 20:20:26 +0300

    fix: replace Link elements with useNavigate to avoid nested <a> issues

--- apps/client/src/components/SharedPostPreview.tsx
+++ apps/client/src/components/SharedPostPreview.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import type { Post } from '@xbookgram/shared';
 
 export function SharedPostPreview({
@@ -7,30 +7,37 @@ export function SharedPostPreview({
 }: {
   post: NonNullable<Post['originalPost']>;
 }) {
+  const navigate = useNavigate();
   return (
-    <Link to={`/posts/${post.id}`}>
-      <Card className="bg-muted text-sm text-muted-foreground">
-        <CardHeader className="flex items-center gap-2">
-          <div className="flex items-center justify-center h-8 w-8 rounded-full bg-blue-500 text-white ">
-            {post.author.username[0].toUpperCase()}
-          </div>
-          <Link to={`/users/${post.author.id}`}>
-            <div className="font-bold">
-              {post.author.username} <span>wrote:</span>
-            </div>
-          </Link>
-          <div className="ml-auto text-muted-foreground">
-            {new Date(post.createdAt).toLocaleDateString('en-US', {
-              month: 'short',
-              day: 'numeric',
-            })}
-          </div>
-        </CardHeader>
-        <CardContent className="pl-10 ">
-          {post.content.slice(0, 150) +
-            (post.content!.length > 150 ? '...' : '')}
-        </CardContent>
-      </Card>
-    </Link>
+    <Card
+      className="bg-muted text-sm text-muted-foreground cursor-pointer"
+      onClick={() => navigate(`/posts/${post.id}`)}
+    >
+      <CardHeader className="flex items-center gap-2">
+        <div className="flex items-center justify-center h-8 w-8 rounded-full bg-blue-500 text-white ">
+          {post.author.username[0].toUpperCase()}
+        </div>
+
+        <div
+          className="font-bold cursor-pointer hover:underline"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate(`/users/${post.author.id}`);
+          }}
+        >
+          {post.author.username} <span>wrote:</span>
+        </div>
+
+        <div className="ml-auto text-muted-foreground">
+          {new Date(post.createdAt).toLocaleDateString('en-US', {
+            month: 'short',
+            day: 'numeric',
+          })}
+        </div>
+      </CardHeader>
+      <CardContent className="pl-10 ">
+        {post.content.slice(0, 150) + (post.content!.length > 150 ? '...' : '')}
+      </CardContent>
+    </Card>
   );
 }
