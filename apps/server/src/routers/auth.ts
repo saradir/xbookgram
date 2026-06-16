b5647050a5a92fba3b854739b5a75c5b39aa126f
saradir <120840462+saradir@users.noreply.github.com>
2026-06-16 12:46:55 +0300

    fix: change sameSite property  to none

--- apps/server/src/routers/auth.ts
+++ apps/server/src/routers/auth.ts
@@ -35,7 +35,7 @@ authRouter.get(
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
